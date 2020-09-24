b2af7ba92e5c4141251e80c695139490357a1ac7
Juan Rios <mrdaniel19996@gmail.com>
2020-09-24 15:08:53 -0500

    [UPDATE] html formt and docs dir

---
+++ docs/precache-manifest.2cc5db3ea4c78369b51f1f8d847df54b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30637c9569bb023d798e607fdcff486",
+    "url": "/index.html"
+  },
+  {
+    "revision": "956c678b4489ed1cdb79",
+    "url": "/static/css/main.85804039.chunk.css"
+  },
+  {
+    "revision": "ccd9761b7db298367323",
+    "url": "/static/js/2.dab6626b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dab6626b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "956c678b4489ed1cdb79",
+    "url": "/static/js/main.216b053d.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
